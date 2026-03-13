09d4f16225c6d8593e9d13549bfa857cd69b07ab
hmn274-ux <hmn274@yonsei.ac.kr>
2026-03-13 05:05:46 +0000

    c

--- lec0/1st.c
+++ lec0/1st.c
@@ -1,6 +1,15 @@
 #include<stdio.h>
 
 int main() {
-    printf("\nhello, world!\n");
+    int a = ;
+    printf("a %%d: %d\n"
+, a);
+printf("a %%i: %i\n"
+, a);
+printf("a %%c: %c\n"
+, a);
+printf("a %%f: %f\n"
+, a);
+printf("-----\n");
     return 0;
 }
